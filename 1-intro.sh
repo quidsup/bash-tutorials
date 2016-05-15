2859ceb836a121c18008685826f2652eca562caf
quidsup <ooop@btinternet.com>
2016-05-15 17:08:30 +0100

    added extra comments

--- 1-intro.sh
+++ 1-intro.sh
@@ -4,3 +4,6 @@
 #v0.1
 
 echo "Hello World" #Write on screen
+
+#do chmod 777 1-intro.sh to make this file executable
+#Copy into /usr/local/sbin to make run it from any folder (depending on the Linux distro)
