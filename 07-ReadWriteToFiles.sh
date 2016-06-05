c5c8d6acf18cff1b243948bb2114fb8632feb8bc
quidsup <ooop@btinternet.com>
2016-06-05 10:55:30 +0100

    Create 07-ReadWriteToFiles.sh

---
+++ 07-ReadWriteToFiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Name: Bash Tutorial 6 Reading / Writing to Files
+#Description: Tutorial shows how to declare and use Numeric and Associative Arrays
+#Video: 
+
+#Simple redirection using < >
+
